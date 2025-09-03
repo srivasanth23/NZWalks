7875d931507bb59d725f1d89e27392f9685e9500
srivasanth23 <jammulasrivasanth@gmial.com>
2025-09-03 22:05:07 +0530

    Image Upload functinallity added

--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -1,19 +1,21 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using NZWalks.API.Data;
 using NZWalks.API.Mappings;
 using NZWalks.API.Repositories;
 using System.Text;
 using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddHttpContextAccessor(); // We are injecting this so that we can consume it in our application
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
@@ -59,6 +61,7 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksAuthConne
 builder.Services.AddScoped<IRegionRepo, SQLRegionRepo>();
 builder.Services.AddScoped<IWalkRepo, SQLWalkRepository>();
 builder.Services.AddScoped<ITokenRepositary, TokenRepo>();
+builder.Services.AddScoped<IImageRepo, LocalImageRepo>();
 
 builder.Services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
 
@@ -115,6 +118,14 @@ app.UseHttpsRedirection();
 
 app.UseAuthentication(); // We are adding because we need to to authenticate before authorization
 app.UseAuthorization();
+
+// Serving Static File 
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Images")),
+    RequestPath = "/Images"
+    // https://localhost:1234/Images
+});
 
 app.MapControllers();
 
---
+++ NZWalks.API/Repositories/LocalImageRepo.cs
@@ -0,0 +1,47 @@
+﻿using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class LocalImageRepo : IImageRepo
+    {
+
+        private readonly IWebHostEnvironment _host;
+        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly NZWalksDbContext _dbContext;
+
+        public LocalImageRepo(IWebHostEnvironment host, 
+            IHttpContextAccessor contextAccessor,
+            NZWalksDbContext dbContext
+            )
+        {
+            _host = host;
+            _contextAccessor = contextAccessor;
+            _dbContext = dbContext;
+        }
+
+
+
+
+        public async Task<Image> Upload(Image image)
+        {
+            var localFilePath = Path.Combine(_host.ContentRootPath, "Images",
+                $"{image.FileName}{image.FileExtension}");
+
+            // Upload Image to Local Path
+            using var stream = new FileStream(localFilePath, FileMode.Create);
+            await image.File.CopyToAsync(stream);
+
+            // https:localhost:7080/images/image.jpeg
+            var urlFilePath = $"{_contextAccessor.HttpContext.Request.Scheme}://{_contextAccessor.HttpContext.Request.Host}{_contextAccessor.HttpContext.Request.PathBase}/Images/{image.FileName}{image.FileExtension}";
+
+            image.FilePath = urlFilePath;
+
+            // Add Image to the Images table
+            await _dbContext.Images.AddAsync(image);
+            await _dbContext.SaveChangesAsync();
+
+            return image;
+        }
+    }
+}
